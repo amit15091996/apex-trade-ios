a1e7ef9ddd58b7baa8658a142f2f8321c46da86f
amit15091996 <amitdewangan199@gmail.com>
2025-09-18 16:33:18 +0530

    first commit

---
+++ src/main/java/com/apex/trade/ios/InverstorOnboardingSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.apex.trade.ios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InverstorOnboardingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InverstorOnboardingSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/trade/ios/InverstorOnboardingSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.trade.ios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InverstorOnboardingSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
